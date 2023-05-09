49fcad6869ef5980d5b76165604d09e551e1b30c
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-05-09 22:45:50 +0200

    make it a api

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.use('/api-docs', express.static(swaggerUi.getAbsoluteFSPath()));
 
-router.get('/', function(req, res, next) {
+router.get('/hello', function(req, res, next) {
   res.send('respond with a resource');
 });
 
