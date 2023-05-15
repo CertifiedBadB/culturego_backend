b378ae0070adc0fb39f61d8b5e65aaeadbba93b7
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-05-15 09:36:32 +0200

    master ready

--- routes.js
+++ routes.js
@@ -18,7 +18,7 @@ const options = {
         openapi:'3.0.0',
         info : {
             title:'Culture Go API',
-            description: 'CultureGo is een toonaangevende API die specifiek ontwikkeld is voor de stad Bergen op Zoom. Het biedt gebruikers een naadloze manier om de culturele aanbiedingen van de stad te ontdekken en te beleven op een unieke manier. Of je nu een inwoner bent die op zoek is naar nieuwe culturele activiteiten of een bezoeker die meer wil weten over de lokale culturele scène, CultureGo heeft alles wat je nodig hebt. Onze API biedt toegang tot een uitgebreide database aan culturele evenementen, tentoonstellingen en attracties in Bergen op Zoom, zodat je gemakkelijk je volgende culturele avontuur in de stad kunt plannen. CultureGo maakt het nog makkelijker om je te verdiepen in de rijke culturele ervaringen van Bergen op Zoom.',
+            description: 'CultureGo api',
             version: '3.0.0'
         },
     },
