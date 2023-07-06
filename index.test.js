84e981de9360e4d0c47861cff2b4b96f3f636351
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-07-06 10:42:25 +0200

    test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require('mongodb');
-
+const {UserController} = require('./controllers/userController')
 describe('insert', () => {
   let connection;
   let db;
@@ -26,3 +26,37 @@ describe('insert', () => {
     expect(insertedUser).toEqual(mockUser);
   });
 });
+
+// spy on method in service
+const mockSaveProduct = jest.spyOn(UserController, 'saveProduct')
+// dummy product
+const mockProductObject = { email: 'dummy@dummy.nl', password: '123@12OO' };
+// result is defined in the format we return in APIs
+const mockProduct = jest.fn(async () => {
+  return { data: mockProductObject };
+});
+// mocking method implementation
+mockSaveProduct.mockImplementation(mockProduct);
+
+const response = httpMocks.createResponse();
+  const request = httpMocks.createRequest();
+  request.body = { email: 'dummy@dummy.nl', password: '123@12OO' };
+
+  it('should create a product', async () => {
+    const response = httpMocks.createResponse();
+    const request = httpMocks.createRequest();
+    request.body = {
+      name: 'dummy 1',
+      price: 10
+    };
+    const mockProduct = jest.fn(async () => {
+      return { data: mockProductObject };
+    });
+    mockSaveProduct.mockImplementation(mockProduct);
+    await UserController.signup_post(request, response);
+    expect(mockSaveProduct).toHaveBeenCalledTimes(1);
+    expect(mockSaveProduct).toHaveBeenCalledWith(mockProductObject);
+    expect(response.statusCode).toEqual(201);
+    expect(response._isEndCalled()).toBeTruthy();
+    expect(response._getJSONData().data.name).toEqual('dummy 1');
+  });
