f2536a4cc42fc226768ba324c39f026f4051f1d8
Jordy Huijgens <133045582+CertifiedBadB@users.noreply.github.com>
2023-07-06 10:53:52 +0200

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,62 +1,77 @@
-const {MongoClient} = require('mongodb');
-const {UserController} = require('./controllers/userController')
-describe('insert', () => {
-  let connection;
-  let db;
+const { signup_post } = require('./controllers/userController');
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db(globalThis.__MONGO_DB_NAME__);
-  });
+describe('signup_post', () => {
+  let req;
+  let res;
 
-  afterAll(async () => {
-    await connection.close();
-  });
-
-  it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
-
-    const mockUser = {_id: 'some-user-id', name: 'John'};
-    await users.insertOne(mockUser);
-
-    const insertedUser = await users.findOne({_id: 'some-user-id'});
-    expect(insertedUser).toEqual(mockUser);
-  });
-});
-
-// spy on method in service
-const mockSaveProduct = jest.spyOn(UserController.signup_post, 'res')
-// dummy product
-const mockProductObject = { email: 'dummy@dummy.nl', password: '123@12OO' };
-// result is defined in the format we return in APIs
-const mockProduct = jest.fn(async () => {
-  return { data: mockProductObject };
-});
-// mocking method implementation
-mockSaveProduct.mockImplementation(mockProduct);
-
-const response = httpMocks.createResponse();
-  const request = httpMocks.createRequest();
-  request.body = { email: 'dummy@dummy.nl', password: '123@12OO' };
-
-  it('should create a product', async () => {
-    const response = httpMocks.createResponse();
-    const request = httpMocks.createRequest();
-    request.body = {
-      name: 'dummy 1',
-      price: 10
+  beforeEach(() => {
+    req = {
+      body: {
+        email: 'dummy@dummy.nl',
+        password: '123@12OO'
+      }
     };
-    const mockProduct = jest.fn(async () => {
-      return { data: mockProductObject };
-    });
-    mockSaveProduct.mockImplementation(mockProduct);
-    await UserController.signup_post(request, response);
-    expect(mockSaveProduct).toHaveBeenCalledTimes(1);
-    expect(mockSaveProduct).toHaveBeenCalledWith(mockProductObject);
-    expect(response.statusCode).toEqual(201);
-    expect(response._isEndCalled()).toBeTruthy();
-    expect(response._getJSONData().data.name).toEqual('dummy 1');
+    res = {
+      status: jest.fn(() => res),
+      json: jest.fn()
+    };
   });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should create a user and return a token', async () => {
+    const createToken = jest.fn().mockReturnValue('dummy-token');
+    const User = {
+      create: jest.fn().mockResolvedValue({ _id: 'some-user-id' })
+    };
+
+    const expectedUser = { user: 'some-user-id' };
+
+    await signup_post(req, res);
+
+    expect(User.create).toHaveBeenCalledTimes(1);
+    expect(User.create).toHaveBeenCalledWith({
+      email: 'dummy@dummy.nl',
+      password: '123@12OO'
+    });
+
+    expect(createToken).toHaveBeenCalledTimes(1);
+    expect(createToken).toHaveBeenCalledWith('some-user-id');
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(201);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(expectedUser);
+  });
+
+  it('should handle errors and return error messages', async () => {
+    const error = new Error('Invalid email');
+    const handleErrors = jest.fn().mockReturnValue(['Invalid email']);
+
+    const User = {
+      create: jest.fn().mockRejectedValue(error)
+    };
+
+    const expectedErrors = ['Invalid email'];
+
+    await signup_post(req, res);
+
+    expect(User.create).toHaveBeenCalledTimes(1);
+    expect(User.create).toHaveBeenCalledWith({
+      email: 'dummy@dummy.nl',
+      password: '123@12OO'
+    });
+
+    expect(handleErrors).toHaveBeenCalledTimes(1);
+    expect(handleErrors).toHaveBeenCalledWith(error);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(expectedErrors);
+  });
+});
