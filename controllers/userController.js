e9a9db9f4eaac8ea605c029b6bb4a3abc572426c
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-07-06 14:59:22 +0200

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,76 +1,63 @@
-const User = require("../model/User");
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-//handleerrors
-const handleErrors= (err) =>{
-    let errors = {email: '', password: '', photo: ''}
+const { signup_post } = require('./userController');
+const User = require('./model/User');
 
-    if (err.message === "verkeerde email"){
-        errors.email = "Ingevoerde gegevens kloppen niet."
-    }
-    if (err.message === "verkeerde wachtwoord"){
-        errors.email = "Ingevoerde gegevens kloppen niet."
-    }
-    //duplicate error code
-if(err.code === 11000){
-    errors.email = "Dit email adres heeft al een account bij ons"
-    return errors;
-}
-    //validate errors 
-    if(err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(({properties}) =>{
-            errors[properties.path] = properties.message
-        })
-    }
-    return errors
-}
+describe('Signup', () => {
+  beforeEach(() => {
+    // Clear the user collection before each test
+    User.deleteMany({});
+  });
 
-const maxAge = 3 * 24 * 60 * 60;
-const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn:maxAge});
-}
+  it('should create a new user and return the user ID', async () => {
+    const req = {
+      body: {
+        email: 'test@example.com',
+        password: 'password',
+        photo: 'photo-url',
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
+    await signup_post(req, res);
 
-module.exports.signup_post = async(req, res) => {
-    const {email, password, photo} = req.body;
-    try{
-        const user = await User.create({email, password, photo});
-        const token = createToken(user._id);
-        res.status(201).json({user: user._id})
-    }
-    catch(err){
-        const errors = handleErrors(err);
-        res.status(400).json(errors);
-    }
-}
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith({ user: expect.any(String) });
 
-module.exports.logout_get = (req, res) => {
-    res.status('jwt','',{maxAge:1});
-}
+    // Check if the user is created in the database
+    const user = await User.findOne({ email: 'test@example.com' });
+    expect(user).toBeDefined();
+    expect(user.email).toBe('test@example.com');
+    expect(user.password).toBe('password');
+    expect(user.photo).toBe('photo-url');
+  });
 
+  it('should handle errors and return error messages', async () => {
+    // Create a user with the same email to simulate duplicate error
+    await User.create({
+      email: 'test@example.com',
+      password: 'password',
+      photo: 'photo-url',
+    });
 
-module.exports.login_post = async(req, res) => {
-    const {email, password} = req.body;
-    try{
-        const user = await User.login(email,password);
-        const token = createToken(user._id);
-        res.status(200).json({user:user._id , token: token});
-    }
-    catch(err){
-        const errors = handleErrors(err);
-        res.status(400).json({errors});
-    }
-}
+    const req = {
+      body: {
+        email: 'test@example.com',
+        password: 'password',
+        photo: 'photo-url',
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-module.exports.getById = async (req, res) => {
-    const { id } = req.body;
-    try {
-      const user = await User.findById(id);
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      return res.json(user);
-    } catch (err) {
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  };
+    await signup_post(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      email: 'Dit email adres heeft al een account bij ons',
+    });
+  });
+});
