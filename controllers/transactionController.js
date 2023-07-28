091bdf6a2334849d7ca2d7ec938679a95692c796
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-07-28 17:35:22 +0200

    transactions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -3,37 +3,35 @@ const User = require ('../model/User');
 const MailingController = require('./mailingController')
 
 
-module.exports.transaction_post = async(req, res) => {
-    const { user,pascode } = req.body;
-    
+module.exports.transaction_post = async (req, res) => {
+    const { user, pascode } = req.body;
+  
     try {
-        const userGet = await User.findById(user);
-        if (!userGet) {
-          return res.status(404).json({ error: 'User not found' });
+      const userGet = await User.findById(user);
+      if (!userGet) {
+        return res.status(404).json({ error: 'User not found' });
+      } else {
+        try {
+          const transaction = new Transaction({
+            points: userGet.points,
+            user: user,
+          });
+          await User.findOneAndUpdate(
+            { _id: user }, // Fix: Use 'user' instead of 'id'
+            { points: 0 }, // Update points
+            { new: true } // Return the updated document
+          );
+          await transaction.save();
+          MailingController.transaction_postmail();
+          return res.send(transaction);
+        } catch (err) {
+          return res.status(400).send(err);
         }
-        else{
-              try {
-                const transaction = new Transaction({
-                    points: userGet.points,
-                    user: user,
-                  });
-                await User.findOneAndUpdate(
-                    { _id: id }, // Filter by user ID
-                    { points: 0}, // Update points 
-                    { new: true } // Return the updated document
-                  );
-                await transaction.save();
-                MailingController.transaction_postmail();
-                return res.send(transaction);
-
-              } catch (err) {
-                return res.status(400).send(err);
-              }
-        }
-    }catch (err) {
-        return res.status(400).send(err);
       }
-    };
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  };
 
 
 
