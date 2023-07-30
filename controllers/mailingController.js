dc60d6282ff67584474b2bfcacf9e80ab066d1a1
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-07-30 11:54:37 +0200

    transactions

--- controllers/mailingController.js
+++ controllers/mailingController.js
@@ -1,27 +1,31 @@
-var nodemailer = require('nodemailer');
-
+const fs = require('fs');
+const path = require('path');
+const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.MAILADRESS,
-        pass: process.env.MAILPASSWORD
-    }
-    });
+  service: 'gmail',
+  auth: {
+    user: process.env.MAILADRESS,
+    pass: process.env.MAILPASSWORD
+  }
+});
 
-module.exports.transaction_postmail = async() => {
-    var mailOptions = {
-        from: process.env.MAILADRESS,
-        to: process.env.MAILADRESS,
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-      }
-    await transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        return error;
-    } else {
-        return ('Email sent: ' + info.response);
-    }
-    })
-}
-//TEST
+module.exports.transaction_postmail = async () => {
+  try {
+    // Read the HTML template from the "assets" folder
+    const templatePath = path.join(__dirname, 'mails', 'puntenoverzetten.html');
+    const htmlTemplate = fs.readFileSync(templatePath, 'utf8');
+
+    const mailOptions = {
+      from: process.env.MAILADRESS,
+      to: process.env.MAILADRESS,
+      subject: 'Gefeliciteerd!',
+      html: htmlTemplate, // Use the HTML template
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    return 'Email sent: ' + info.response;
+  } catch (error) {
+    return error;
+  }
+};
