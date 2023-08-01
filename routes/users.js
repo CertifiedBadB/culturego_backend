a0179b167652ab21ebdeda74d6ded39356e9cf59
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-08-01 10:28:30 +0200

    passwordreset

--- routes/users.js
+++ routes/users.js
@@ -148,5 +148,75 @@ router.post('/getById',verifyToken, userController.getById);
  */
 router.post('/updatePoints',verifyToken, userController.getByIdAndUpdatePoints);
 
+/**
+ * @swagger
+ * /users/passwordreset1:
+ *  post:
+ *      tags:
+ *        - users (authorization)
+ *      summary: Use this call to get a password reset token
+ *      description: The body needs an email
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                              description: Email of the user
+ *                              example: "user@example.com"
+ *                              required: true
+ *      responses:
+ *          200:
+ *              description: Successful login
+ *          400:
+ *              description: Bad request
+ *          401:
+ *              description: Unauthorized
+ */
+router.post('/passwordreset1', userController.passwordreset1);
+
+/**
+ * @swagger
+ * /users/passwordreset2:
+ *  post:
+ *      tags:
+ *        - users (authorization)
+ *      summary: Use this call to reset the password
+ *      description: The body needs an email, token and newpassword
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                              description: Email of the user
+ *                              example: "user@example.com"
+ *                              required: true
+ *                          token:
+ *                              type: string
+ *                              description: token of the user
+ *                              example: "uUhGr5"
+ *                              required: true
+ *                          newPassword:
+ *                              type: string
+ *                              description: password of the user
+ *                              example: "uUhGr5"
+ *                              required: true
+ *      responses:
+ *          200:
+ *              description: Successful login
+ *          400:
+ *              description: Bad request
+ *          401:
+ *              description: Unauthorized
+ */
+router.post('/passwordreset2', userController.passwordreset2);
+
 router.get('/logout', userController.logout_get);
 module.exports = router;
