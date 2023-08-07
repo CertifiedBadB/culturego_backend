74cdaf70382b6235465ed8ed6ccffb0fd1c71867
Jordy Huijgens <jordy@klaassenontwerp.nl>
2023-08-07 16:37:06 +0200

    ds

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,7 @@ module.exports.signup_post = async (req, res) => {
   } catch (err) {
     console.error('Error:', err);
     const errors = handleErrors(err);
-    res.status(400).json(errors);
+    res.status(400).json(errors + err);
   }
 };
 
